642d2fc0f661031600e726e162e5ff330d22956f
Paolo Secci <31908549+paolosecci@users.noreply.github.com>
2018-08-01 20:50:51 -0700

    hw-for-social-analytics

---
+++ aint_nothing_on_the_news_but_the_blues/config.py
@@ -0,0 +1,4 @@
+consumer_key = 'ZeBQvBc2W0O3nIi2qLMmhM009'
+consumer_secret = 'bYCjvrbd9OPAXUWCIAMtltumZwdqTbpxE7Se4p5zuBhq2Io7i9'
+access_token = '1654896086-Gq7SnW9kFQNQdPM99uo4TZGbWKsDcCalR4IeYjX'
+access_token_secret = 'YXlKf6YcYitTsYorTS4wc0gdJf0jxUWDMFxGBFvgQ5SUF'
